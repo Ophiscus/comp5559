e0d5e31387b28da4a61ffbc27a2ecfc60392480a
Kurt Santos <krs23@kent.ac.uk>
2022-03-09 17:13:09 +0000

    Uploaded receptionists sql file

---
+++ co559_receptionists.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: co559
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `receptionists`
+--
+
+DROP TABLE IF EXISTS `receptionists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `receptionists` (
+  `receptionistID` int NOT NULL AUTO_INCREMENT,
+  `receptionistForename` longtext NOT NULL,
+  `receptionistSurname` longtext NOT NULL,
+  `receptionistDoB` longtext NOT NULL,
+  `receptionistPhoneNumber` longtext NOT NULL,
+  `receptionistUsername` longtext NOT NULL,
+  `receptionistPassword` longtext NOT NULL,
+  PRIMARY KEY (`receptionistID`),
+  UNIQUE KEY `receptionistID_UNIQUE` (`receptionistID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receptionists`
+--
+
+LOCK TABLES `receptionists` WRITE;
+/*!40000 ALTER TABLE `receptionists` DISABLE KEYS */;
+INSERT INTO `receptionists` VALUES (1,'Carol','Hampton','01/02/2002','+44 115 496 0556','ch1','password1'),(2,'Vicki','Bennett','19/02/1993','+44 115 496 0321','vb2','password2');
+/*!40000 ALTER TABLE `receptionists` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-09 17:09:11
