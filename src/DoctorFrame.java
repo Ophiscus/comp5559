4a345cf4fde79f5240dee60b697f469abe353375
Joel Kimani <jk626@kent.ac.uk>
2022-03-25 23:49:27 +0000

    Replace DoctorFrame.java

--- src/DoctorFrame.java
+++ src/DoctorFrame.java
@@ -16,7 +16,7 @@ public class DoctorFrame extends JFrame implements ActionListener
    private Container container= getContentPane();
    private JLabel docForenameLabel=new JLabel("Forename");
    private JLabel docSurnameLabel=new JLabel("Surname");
-   private JLabel docDOBLabel=new JLabel("Date of birth");
+   private JLabel docDOBLabel=new JLabel("Date of birth (yyyy-MM-dd)");
    private JLabel docSpecLabel=new JLabel("Speciality");
    private JLabel docPhoneNumberLabel=new JLabel("Phone Number");
    private JTextField forenameField=new JTextField();
@@ -26,8 +26,7 @@ public class DoctorFrame extends JFrame implements ActionListener
    private JTextField phoneNumberField=new JTextField();
    private JButton addButton=new JButton("ADD");
    private JButton resetButton=new JButton("RESET");
-   private JLabel titleLabel=new JLabel("Doctor registation form");
-   private int count = 6;
+   private JLabel titleLabel=new JLabel("Doctor registration form");
     DoctorFrame()
     {
         setLocationAndSize();
@@ -42,18 +41,18 @@ public class DoctorFrame extends JFrame implements ActionListener
     }
     public void setLocationAndSize()
     {
-        //Setting location and Size of each components using setBounds() method.
+        //Setting location and Size of each component using setBounds() method.
     	titleLabel.setBounds(100,50,150,60);
         docForenameLabel.setBounds(50,100,100,30);
-        forenameField.setBounds(150,100,150,30);
+        forenameField.setBounds(250,100,150,30);
         docSurnameLabel.setBounds(50,150,100,30);
-        surnameField.setBounds(150,150,150,30);
+        surnameField.setBounds(250,150,150,30);
         docSpecLabel.setBounds(50,200,100,30);
-        specialityField.setBounds(150,200,150,30);
+        specialityField.setBounds(250,200,150,30);
         docPhoneNumberLabel.setBounds(50,250,150,30);
-        phoneNumberField.setBounds(150,250,150,30);
-        docDOBLabel.setBounds(50,300,100,30);
-        dateOfBirthField.setBounds(150,300, 150,30);
+        phoneNumberField.setBounds(250,250,150,30);
+        docDOBLabel.setBounds(50,300,175,30);
+        dateOfBirthField.setBounds(250,300, 150,30);
         addButton.setBounds(50,500,100,30);
         resetButton.setBounds(200,500,100,30);
     }
@@ -73,7 +72,7 @@ public class DoctorFrame extends JFrame implements ActionListener
        container.add(docPhoneNumberLabel);
        container.add(phoneNumberField);
     }
-    
+
     public void addActionEvent()
     {
        //adding Action listener to components
@@ -94,40 +93,31 @@ public class DoctorFrame extends JFrame implements ActionListener
 
          if(e.getSource() == addButton)
          {
-             String doctorForename = forenameField.getText();
-             String doctorSurname = surnameField.getText();
-             String doctorSpeciality = specialityField.getText();
-             String doctorDoB = dateOfBirthField.getText();
-             String doctorPhoneNumber = phoneNumberField.getText();
-             String number = Integer.toString(count);
-             String insertSql = "INSERT INTO c0559.doctors-table ( "+ number + ", " + doctorForename + ", " + doctorSurname + ", " + doctorSpeciality + ", " + doctorDoB + "," + doctorPhoneNumber +" );";
+             String Forename = forenameField.getText();
+             String Surname = surnameField.getText();
+             String Speciality = specialityField.getText();
+             Date DoB=Date.valueOf(dateOfBirthField.getText());
+             String phoneNumber = phoneNumberField.getText();
+
              try {
                  Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/c0559", "root", "Dark Shadow");
-                 Statement statement = connection.createStatement();
-                 ResultSet resultSet = null;
-                 PreparedStatement prepsInsertProduct = connection.prepareStatement(insertSql,Statement.RETURN_GENERATED_KEYS);
-                 prepsInsertProduct.execute();
-                 resultSet = prepsInsertProduct.getGeneratedKeys();
+                 PreparedStatement stmt= connection.prepareStatement("INSERT INTO c0559.doctors(doctorForename , doctorSurname ,doctorSpeciality,doctorDoB,doctorPhoneNumber) VALUES(?,?,?,?,?)");
+                 stmt.setString(1,Forename);
+                 stmt.setString(2,Surname);
+                 stmt.setString(3,Speciality);
+                 stmt.setDate(4,DoB);
+                 stmt.setString(5,phoneNumber);
+                 stmt.executeUpdate();
+                 JOptionPane.showMessageDialog(this, "Successful");
              }
              catch(Exception f)
              {
                  f.printStackTrace();
+                 JOptionPane.showMessageDialog(this, "Invalid entry. Please re-check details entered");
              }
-            count ++;
 
          }
     }
 
-    public void sql()
-    {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/c0559", "root", "Dark Shadow");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = null;
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+
 }
